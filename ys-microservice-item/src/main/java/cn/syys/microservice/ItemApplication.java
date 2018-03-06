6e22df5b367b2330aad271e14db6d73fc39809c5
yangsong0925 <13980152803@163.com>
2018-03-06 17:02:02 +0800

    初次提交

---
+++ ys-microservice-item/src/main/java/cn/syys/microservice/ItemApplication.java
@@ -0,0 +1,31 @@
+/**
+ * Copyright (C), 2015-2018, XXX有限公司
+ * FileName: ItemApplication
+ * Author:   Administrator
+ * Date:     2018/3/6 14:02
+ * Description: Item Spring Boot 入口
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package cn.syys.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈Item Spring Boot 入口〉
+ *
+ * @author Administrator
+ * @create 2018/3/6
+ * @since 1.0.0
+ */
+@SpringBootApplication
+public class ItemApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(ItemApplication.class,args);
+    }
+
+}
