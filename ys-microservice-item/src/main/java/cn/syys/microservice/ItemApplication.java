5058c8210339e464bbb1819b571f2adced556cbd
yangsong0925 <yangsong563917>
2018-03-07 23:32:13 +0800

    zuul 更新

--- ys-microservice-item/src/main/java/cn/syys/microservice/ItemApplication.java
+++ ys-microservice-item/src/main/java/cn/syys/microservice/ItemApplication.java
@@ -13,6 +13,7 @@ package cn.syys.microservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * 〈一句话功能简述〉<br> 
@@ -22,7 +23,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  * @create 2018/3/6
  * @since 1.0.0
  */
-@EnableDiscoveryClient
+@EnableDiscoveryClient   //通过该注解，实现服务发现，注册
 @SpringBootApplication
 public class ItemApplication {
 
